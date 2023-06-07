80eca6cf68dc136367571049f8b88092243353d1
Daniel <jdvela158@gmail.com>
2023-06-06 19:17:17 -0600

    Clase 4

--- Clase 3/index.js
+++ Clase 3/index.js
@@ -3,8 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+    
+    getMovies();
+    console.log("Carrie");
+    
     res.send("Hola a todos");
 });
+
+function getMovies() {
+    setTimeout(() => {
+        console.log("Scarface");
+    }, 2000);
+}
 
 app.listen(port, () => {
     console.log("Server on port ", port);
